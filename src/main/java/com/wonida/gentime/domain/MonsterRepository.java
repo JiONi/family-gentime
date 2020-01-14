4a4ed6a741387fb5755da508af26e4aeded926ba
JiONi <uphp179@gmail.com>
2020-01-14 20:23:38 +0900

    젠타임 영역 정렬 수정

--- src/main/java/com/wonida/gentime/domain/MonsterRepository.java
+++ src/main/java/com/wonida/gentime/domain/MonsterRepository.java
@@ -24,7 +24,7 @@ public interface MonsterRepository extends JpaRepository<Monster, Long> {
     @Query("SELECT m " +
             "FROM Monster m " +
             "WHERE m.genTerm between (?1-10) and ?1 " +
-            "ORDER BY m.genTerm, m.genTime")
+            "ORDER BY m.genTime")
     Stream<Monster> findAllByGenTerm(int genTerm);
 
     @Modifying
